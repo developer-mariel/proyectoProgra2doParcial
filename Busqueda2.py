e40defe206fbdc7d6a00bf10fd988b1378767919
emmanuelterrazas07-star <emmanuelterrazas07@gmail.com>
2026-04-18 22:04:04 -0600

    Add files via upload

---
+++ Busqueda2.py
@@ -0,0 +1,18 @@
+alumnos = {394704: "Carlos Terrazas",394611: "Mariel Rivas",394570: "Ricardo Robles",
+394412: "Elias Campos"}
+
+while True:
+    print("--- BUSQUEDA DE ALUMNO ---")
+
+    mat = int(input("Ingresa la matricula (0 para salir): "))
+
+    if mat == 0:
+        print("Saliendo de la busqueda...")
+        break
+
+    if mat in alumnos:
+        print("Alumno encontrado")
+        print("Nombre:", alumnos[mat])
+        print("Matricula:", mat)
+    else:
+        print("El alumno no esta en el equipo")
