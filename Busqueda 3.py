0f83cea7add60dc9d94e0ba373df3748f0bf2575
emmanuelterrazas07-star <emmanuelterrazas07@gmail.com>
2026-04-26 09:15:01 -0600

    Add files via upload

---
+++ Busqueda 3.py
@@ -0,0 +1,26 @@
+alumnos = {
+    394704: "Carlos Terrazas",
+    394611: "Mariel Rivas",
+    394570: "Ricardo Robles",
+    394412: "Elias Campos"
+}
+
+while True:
+    print("--- BUSQUEDA DE ALUMNO ---")
+
+    try:
+        mat = int(input("Ingresa la matricula (0 para salir): "))
+    except:
+        print("Error: debes ingresar un número")
+        continue   # regresa al inicio del while
+
+    if mat == 0:
+        print("Saliendo de la busqueda...")
+        break
+
+    if mat in alumnos:
+        print("Alumno encontrado")
+        print("Nombre:", alumnos[mat])
+        print("Matricula:", mat)
+    else:
+        print("El alumno no esta en el equipo")
